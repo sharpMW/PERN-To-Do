da899d2170384225e55c298c366a29ada551253c
sharpMW <saarimmw@gmail.com>
2022-06-25 20:04:45 +0530

    scripts, sequelize, env

---
+++ server/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+        postgres : {
+            options: {
+                host: process.env.HOST,
+                port: process.env.DB_PORT,
+                database: process.env.DB_NAME,
+                dialect: 'postgres',
+                username: process.env.USERNAME,
+                password: process.env.DB_PASSWORD
+            }
+        }
+    }
+}
